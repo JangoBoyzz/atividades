c894885275dfee4720c5e746caecfe5d46c6120e
Jango <160745507+JangoBoyzz@users.noreply.github.com>
2024-04-22 11:19:36 -0300

    Add files via upload
    atividade tela de login

---
+++ atividadeLogin/script/script.js
@@ -0,0 +1,14 @@
+function salvardados(){
+    var nome= document.getElementById('nome').value;
+    var senha= document.getElementById('senha').value;
+    let nomeC = 'jango';
+    let senhaC = '123';
+
+    if ((nome === nomeC) && (senha === senhaC)){
+        window.location.href  = "outra.html"
+    }else{
+        alert ("O usuario ou senha está incorreta!");
+    }
+
+    
+}
